69b32044a20395993f6e258a8d52df523e8697c2
Ryan Roler <ryan.roler@gmail.com>
2016-09-09 18:24:49 -0700

    Updated api.py to use flask-restful. Only test still passes after transition to api.DummyResource from api.main

--- api.py
+++ api.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
+
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def main():
-    return ''
+class DummyResource(Resource):
+    def get(self):
+        return {}
+
+
+api.add_resource(DummyResource, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
