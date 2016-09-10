bfeaef73189ed1253b08162b6a773ef8faff8e96
Ryan Roler <ryan.roler@gmail.com>
2016-09-09 23:30:17 -0700

    Had to revise the test: why make sure that a config file has a key? I've got two basics now that ensure that the class is configured, and that the accepted_extensions has contents. Set the instantiation of the class to a fixture, but I may change that to ensure that caching of configs works between instances.

--- libs/source_managers.py
+++ libs/source_managers.py
@@ -1,6 +1,19 @@
-class DirectLinkManager():
-    def __init__(self):
-        pass
+import configparser
+CONFIG_FILE = '../config.ini'
 
-    def configure(self):
-        pass
+
+class DirectLinkManager():
+    _config = None
+
+    def __init__(self):
+        if self._config is None:
+            self.configure()
+
+    @classmethod
+    def configure(cls):
+        cls._config = configparser.ConfigParser()
+
+        with open(CONFIG_FILE) as source:
+            cls._config.read_file(source)
+        extensions = cls._config.get('directlink', 'AcceptedExtensions')
+        cls.accepted_extensions = extensions.split(',')
