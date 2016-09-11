6e219aa1d183db5ffbf9f97874d004fddc920ec0
Ryan Roler <ryan.roler@gmail.com>
2016-09-10 23:00:13 -0700

    Added comments to the tests for DirectLinkManager, altered the name of the test for match to reflect the lack of config file, and made is_configured more useful by having it check to ensure that the _config supports the get method (dict or configparser-like)

--- tests/test_libs_source_managers.py
+++ tests/test_libs_source_managers.py
@@ -21,14 +21,17 @@ class TestDirectLink():
         return source_managers.DirectLinkManager()
 
     def test_config_on_instantiation(self, mocker):
+        '''Ensure that the configure method is called on instantiation'''
         mocker.spy(source_managers.DirectLinkManager, 'configure')
         source_managers.DirectLinkManager()
         assert source_managers.DirectLinkManager.configure.call_count == 1
 
     def test_is_configured(self, manager):
-        assert source_managers.DirectLinkManager._config
+        '''Ensure that the _config attribute has a "get" method'''
+        assert hasattr(source_managers.DirectLinkManager._config, 'get')
 
     def test_accepted_extensions_exists(self, manager):
+        '''Ensure that accepted_extensions is populated on instantiation'''
         assert manager.accepted_extensions
 
     @pytest.mark.parametrize('url,is_match', [
@@ -39,13 +42,15 @@ class TestDirectLink():
         ('http://test.com/test.foo?bar=baz', True),
         ('http://test.com/test.foo?bar=baz&qux', True),
     ])
-    def test_match_extensions_from_config(self, monkeypatch, url, is_match):
+    def test_match(self, monkeypatch, url, is_match):
+        '''Sends a variety of URLs to the match method. Expects ".foo" ext'''
         monkeypatch.setattr(source_managers.DirectLinkManager,
                             'accepted_extensions',
                             ['.foo'])
         assert source_managers.DirectLinkManager.match(url) == is_match
 
     def test_get_images(self, manager):
+        '''DirectLinkManager only returns the passed url, in a generator'''
         url = 'http://test.com/test.foo'
         results = manager.get_images(url)
         assert next(results) == url
