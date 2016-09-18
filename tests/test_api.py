170428138726124b6423201f2cce08a1d22a4d1a
Ryan Roler <ryan.roler@gmail.com>
2016-09-17 18:03:46 -0700

    Creating a test that will send a selection of subreddits to the server. Subs should be joined on '+' like with Reddit, and should follow the root '/', like '/aww+cute', for example.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from rStream.api import app
@@ -11,3 +13,12 @@ def client():
 def test_root_response_is_200(client):
     response = client.get('/')
     assert '200' in response.status
+
+
+def test_subreddit_selection(client):
+    test_subs = ['1', '2', '3']
+    url = '/' + '+'.join(test_subs)
+    response = client.get(url)
+    raw = response.data.decode('utf8')
+    results = json.loads(raw)
+    assert results['SubsSelected'] == test_subs
