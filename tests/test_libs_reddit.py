c9939a234efc478b618fbcbdd87c7aa81ebf4e27
Ryan Roler <ryan.roler@gmail.com>
2016-09-23 17:16:51 -0700

    Adding basic tests for a wrapper that will catch errors when the PRAW lazily evaluated results are actually fetched. I'd like to keep this error handling more centralized than in the RedditImageDownloader.

--- tests/test_libs_reddit.py
+++ tests/test_libs_reddit.py
@@ -1,5 +1,40 @@
+from praw import errors
+
 from rStream.libs import reddit
 
 
-def test_sub_by_name(name):
-    reddit()
+def test_LazilyEvaluatedWrapper():
+    class ToWrap():
+        def request(self):
+            yield from self._first_works()
+            yield from self._raises_ClientException()
+            yield from self._raises_APIException()
+            yield from self._raises_HTTPException()
+            yield from self._last_works()
+
+        def _first_works():
+            yield 0
+
+        def _raises_ClientException(self):
+            raise errors.ClientException()
+
+        def _raises_APIException(self):
+            raise errors.APIException()
+
+        def _raises_HTTPException(self):
+            raise errors.HTTPException()
+
+        def _last_works():
+            yield 1
+
+    wrapped = reddit.LazilyEvaluatedWrapper(ToWrap())
+    result = wrapped.request()
+    assert list(result) == [1, 2]
+
+
+def test_sub_by_name(monkeypatch):
+    def mock_getter(*args, **kwargs):
+        return 'FooBar'
+
+    monkeypatch.setattr(reddit.REDDIT, 'get_subreddit', mock_getter)
+    assert reddit.get_sub_by_name('foo') == 'FooBar'
