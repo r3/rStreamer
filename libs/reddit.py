8a9ac9ffc3e5ebefd71aa505b4a63bf7fe51962b
Ryan Roler <ryan.roler@gmail.com>
2016-09-23 18:08:16 -0700

    Simplified the tests as I'm generalizing the wrapper class. Will accept an item to wrap, and the exceptions to catch. If an exception is raised, a StopIteration is emitted instead to allow for any consuming functions to safely stop iterating over our wrapped object.

--- libs/reddit.py
+++ libs/reddit.py
@@ -6,8 +6,21 @@ REDDIT = praw.Reddit(user_agent=user_agent)
 
 
 class LazilyEvaluatedWrapper():
-    def __init__(self, wrapped):
+    def __init__(self, wrapped, to_catch):
         self.__wrapped = wrapped
+        self.to_catch = to_catch
+
+    def catch_exceptions(self, func, *args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except self.to_catch as err:
+            raise StopIteration(err)
+
+    def __next__(self):
+        return self.catch_exceptions(next, self.__wrapped)
+
+    def __getattr__(self, attr):
+        return self.catch_exceptions(getattr, self.__wrapped, attr)
 
 
 def get_sub_by_name(name):
--- tests/test_libs_reddit.py
+++ tests/test_libs_reddit.py
@@ -1,35 +1,21 @@
 from praw import errors
+import pytest
 
 from rStream.libs import reddit
 
 
-def test_LazilyEvaluatedWrapper():
-    class ToWrap():
-        def request(self):
-            yield from self._first_works()
-            yield from self._raises_ClientException()
-            yield from self._raises_APIException()
-            yield from self._raises_HTTPException()
-            yield from self._last_works()
-
-        def _first_works():
+def test_lazilyevaluatedwrapper():
+    class LazyGeneratorWithErrors():
+        def iter_raises_clientexception(self):
             yield 0
+            raise errors.PRAWException()
 
-        def _raises_ClientException(self):
-            raise errors.ClientException()
+    volatile = LazyGeneratorWithErrors().iter_raises_clientexception()
+    safe = reddit.LazilyEvaluatedWrapper(volatile, errors.PRAWException)
 
-        def _raises_APIException(self):
-            raise errors.APIException()
-
-        def _raises_HTTPException(self):
-            raise errors.HTTPException()
-
-        def _last_works():
-            yield 1
-
-    wrapped = reddit.LazilyEvaluatedWrapper(ToWrap())
-    result = wrapped.request()
-    assert list(result) == [1, 2]
+    assert next(safe) == 0
+    with pytest.raises(StopIteration):
+        next(safe)
 
 
 def test_sub_by_name(monkeypatch):
