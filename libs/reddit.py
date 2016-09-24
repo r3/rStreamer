27a9551c0098695a45c2c125b56ca7f6b518a729
Ryan Roler <ryan.roler@gmail.com>
2016-09-23 20:05:04 -0700

    Reworked LazilyEvaluatedWrapper. It doesn't need to be that complex. I'll need to adjust the tests to reflect these changes, but it'll be for the best, I think. Tests are going to be in a test class, so I've migrated the existing test there, and will expand. All tests currently pass.

--- libs/reddit.py
+++ libs/reddit.py
@@ -6,21 +6,22 @@ REDDIT = praw.Reddit(user_agent=user_agent)
 
 
 class LazilyEvaluatedWrapper():
-    def __init__(self, wrapped, to_catch):
+    def __init__(self, wrapped, to_catch, sentinel=None):
         self.__wrapped = wrapped
         self.to_catch = to_catch
-
-    def catch_exceptions(self, func, *args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except self.to_catch as err:
-            raise StopIteration(err)
+        self.sentinel = sentinel
 
     def __next__(self):
-        return self.catch_exceptions(next, self.__wrapped)
+        try:
+            return next(self.__wrapped)
+        except self.to_catch:
+            raise StopIteration
 
     def __getattr__(self, attr):
-        return self.catch_exceptions(getattr, self.__wrapped, attr)
+        try:
+            return getattr(self.__wrapped, attr)
+        except self.to_catch:
+            return self.sentinel
 
 
 def get_sub_by_name(name):
--- tests/test_libs_reddit.py
+++ tests/test_libs_reddit.py
@@ -4,18 +4,20 @@ import pytest
 from rStream.libs import reddit
 
 
-def test_lazilyevaluatedwrapper():
+class TestLazilyEvaluatedWrapper():
     class LazyGeneratorWithErrors():
         def iter_raises_clientexception(self):
             yield 0
             raise errors.PRAWException()
 
-    volatile = LazyGeneratorWithErrors().iter_raises_clientexception()
-    safe = reddit.LazilyEvaluatedWrapper(volatile, errors.PRAWException)
+    def test_lazilyevaluatedwrapper_as_iter(self):
+        generator = self.LazyGeneratorWithErrors()
+        volatile = generator.iter_raises_clientexception()
+        safe = reddit.LazilyEvaluatedWrapper(volatile, errors.PRAWException)
 
-    assert next(safe) == 0
-    with pytest.raises(StopIteration):
-        next(safe)
+        assert next(safe) == 0
+        with pytest.raises(StopIteration):
+            next(safe)
 
 
 def test_sub_by_name(monkeypatch):
