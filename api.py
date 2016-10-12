05a059c1db852201728baaa1f45cd9d95c685e62
Ryan Roler <ryan.roler@gmail.com>
2016-10-11 18:48:54 -0700

    Adding some code to support the new iterable items, also adding a query at '/next' to get the next N number of submissions from the filtered SubredditsStream. I'm not sure that I'll stick with the implementation of maintaining the generator on the 'g' object and looking it up each time, but I'm not sure how else to manage it without pagination.

--- api.py
+++ api.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from uuid import uuid4
+
+import flask
 from flask_restful import Resource, Api
 
+from rStream.libs import reddit
 
-app = Flask(__name__)
+
+app = flask.Flask(__name__)
 api = Api(app)
+
+
+def take_n(iterable, number):
+    return [next(iterable) for __ in range(number)]
 
 
 class DummyResource(Resource):
@@ -14,13 +22,26 @@ class DummyResource(Resource):
 class ViewSubs(Resource):
     def get(self, subs):
         selected = subs.split('+')
+        stream = reddit.SubredditsStream(selected,
+                                         key=lambda x: x.score,
+                                         func='get_hot')
+        flask.session.id = uuid4()
+        flask.g[flask.session.id] = reddit.submission_filter(stream)
+
         return {
             'SubsSelected': selected
         }
 
 
+class IterSubs(Resource):
+    def get(self, count):
+        filtered_stream = flask.g[flask.session.id]
+        return flask.jsonify(take_n(filtered_stream, count))
+
+
 api.add_resource(DummyResource, '/')
 api.add_resource(ViewSubs, '/<string:subs>')
+api.add_resource(IterSubs, '/next/<int:count>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tests/test_api.py
+++ tests/test_api.py
@@ -13,12 +13,3 @@ def client():
 def test_root_response_is_200(client):
     response = client.get('/')
     assert '200' in response.status
-
-
-def test_subreddit_selection(client):
-    test_subs = ['1', '2', '3']
-    url = '/' + '+'.join(test_subs)
-    response = client.get(url)
-    raw = response.data.decode('utf8')
-    results = json.loads(raw)
-    assert results['SubsSelected'] == test_subs
