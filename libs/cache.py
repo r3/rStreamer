5bec4e1d7e6ff0a7d9287cae4f1eb3277fd2cdda
Ryan Roler <ryan.roler@gmail.com>
2016-11-03 19:00:12 -0700

    Adding a custom in-memory cache to the libs. ExpiringDict acts like a normal dict, but members will be removed if they are not accessed (get/set) within a given timeout. Tests do not exist as this is mostly exploratory programming. Hopefully I'll find a library out there that does this more reasonably. If not, I'll need to finish implementing all of the dict methods so that the ExpiringDict is thread-safe.

---
+++ libs/cache.py
@@ -0,0 +1,63 @@
+import threading
+
+
+TIMEOUT = 10.0  # In seconds
+
+
+class ExpirationTimer(threading.Timer):
+    def __init__(self, timeout, func, *args, **kwargs):
+        self.timeout = timeout
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.reset()
+
+    def reset(self):
+        try:
+            self.__timer.cancel()
+        except AttributeError:
+            pass
+
+        self.__timer = threading.Timer(self.timeout,
+                                       self.func,
+                                       *self.args,
+                                       **self.kwargs)
+        self.__timer.start()
+
+    def start(self):
+        self.__timer.start()
+
+    def cancel(self):
+        self.__timer.cancel()
+
+
+class Expirable():
+    def __init__(self, stream, timeout=None):
+        self.stream = stream
+        self.timeout = timeout
+
+
+class ExpiringDict(dict):
+    def __init__(self, *args, **kwargs):
+        self.lock = threading.Lock()
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, key):
+        with self.lock:
+            value = super().__getitem__(key)
+
+        value.timeout.reset()
+        return value.stream
+
+    def __setitem__(self, key, value):
+        timer = ExpirationTimer(TIMEOUT, self.__delitem__, [key])
+        wrapped = Expirable(value, timer)
+
+        with self.lock:
+            super().__setitem__(key, wrapped)
+
+    def __delitem__(self, key):
+        with self.lock:
+            value = super().__getitem__(key)
+            value.timeout.cancel()
+            super().__delitem__(key)
