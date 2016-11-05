1a711e65aa3386298822b48e03ee4efbb92a32d9
Ryan Roler <ryan.roler@gmail.com>
2016-11-04 19:33:31 -0700

    Added a test to ensure that the timer can be cancelled. Reducing wait times in tests to make the suite run faster

--- tests/test_libs_cache.py
+++ tests/test_libs_cache.py
@@ -1,3 +1,4 @@
+import time
 import threading
 
 from rStream.libs import cache
@@ -10,7 +11,7 @@ class TestExpirationTimer():
             assert kwargs['bar'] == 'qux'
 
         cache.ExpirationTimer(
-            1.0,
+            0.1,
             testfunc,
             ['foo'],
             {'bar': 'qux'}
@@ -24,14 +25,23 @@ class TestExpirationTimer():
         def check_test_timer(timer):
             assert timer.is_alive()
 
-        test_timer = cache.ExpirationTimer(2.0, lambda x: x, [None])
+        test_timer = cache.ExpirationTimer(0.2, lambda x: x, [None])
 
-        reset_test_timer = threading.Timer(1.0, test_timer.reset)
+        reset_test_timer = threading.Timer(0.1, test_timer.reset)
         reset_test_timer.start()
 
-        polling_timer = threading.Timer(3.0, check_test_timer, [test_timer])
+        polling_timer = threading.Timer(0.3, check_test_timer, [test_timer])
         polling_timer.start()
         polling_timer.join()
+
+    def test_cancellation(self):
+        def shouldnt_execute():
+            raise Exception("This executes if cancel method fails")
+
+        timer = cache.ExpirationTimer(2.0, shouldnt_execute)
+        timer.cancel()
+        time.sleep(0.5)  # Takes a moment for cancel to stop the thread
+        assert timer.is_alive() is False
 
 
 def test_Expirable():
